fa03c04268e2a73f1b4d23d5c62fad933461a93d
jonathan <jonathan.henrique@mail.com>
2015-12-10 12:47:53 -0200

    Solution
    Solution-mvc-mvc-basic

---
+++ SonicBliss/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SonicBliss.Startup))]
+namespace SonicBliss
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
